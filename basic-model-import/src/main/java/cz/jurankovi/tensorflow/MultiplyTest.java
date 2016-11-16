1f38a54f901013a8be5ed461123cf507f2161c8c
Vojtech Juranek <vjuranek@redhat.com>
2016-11-16 23:03:02 +0100

    Update basic model

--- basic-model-import/src/main/java/cz/jurankovi/tensorflow/MultiplyTest.java
+++ basic-model-import/src/main/java/cz/jurankovi/tensorflow/MultiplyTest.java
@@ -1,10 +1,10 @@
 package cz.jurankovi.tensorflow;
 
-import static org.bytedeco.javacpp.tensorflow.DT_FLOAT;
+import static org.bytedeco.javacpp.tensorflow.DT_DOUBLE;
 import static org.bytedeco.javacpp.tensorflow.InitMain;
 import static org.bytedeco.javacpp.tensorflow.ReadBinaryProto;
 
-import java.nio.FloatBuffer;
+import java.nio.DoubleBuffer;
 
 import org.bytedeco.javacpp.tensorflow.Env;
 import org.bytedeco.javacpp.tensorflow.GraphDef;
@@ -23,33 +23,28 @@ public class MultiplyTest {
 		InitMain("trainer", (int[]) null, null);
 
 		GraphDef def = new GraphDef();
-		ReadBinaryProto(Env.Default(), "/tmp/my-model/train.pb", def);
+		ReadBinaryProto(Env.Default(), "/tmp/my-model/basic.pb", def);
 		SessionOptions options = new SessionOptions();
 		try (final Session session = new Session(options)) {
+			Status sess  = session.Create(def);
+			
+			Tensor x = new Tensor(DT_DOUBLE, new TensorShape(3, 1));
+			Tensor y = new Tensor(DT_DOUBLE, new TensorShape(3, 1));
+			DoubleBuffer xBuff = x.createBuffer();
+			xBuff.put(new double[] { 2.0, 3.0, 4.0 });
+			DoubleBuffer yBuff = y.createBuffer();
+			yBuff.put(new double[] { 3.0, 4.0, 5.0 });
 
-			Status s = session.Create(def);
-			if (!s.ok()) {
-				throw new RuntimeException(s.error_message().getString());
-			}
-
-			Tensor a = new Tensor(DT_FLOAT, new TensorShape(3, 1));
-			Tensor b = new Tensor(DT_FLOAT, new TensorShape(3, 1));
-			FloatBuffer aVal = a.createBuffer();
-			aVal.put(new float[] { 2.0f, 3.0f, 4.0f });
-			FloatBuffer bVal = b.createBuffer();
-			bVal.put(new float[] { 3.0f, 4.0f, 5.0f });
-
-			Tensor c = new Tensor(DT_FLOAT, new TensorShape(3, 1));
 			TensorVector outputs = new TensorVector();
-			s = session.Run(
-					new StringTensorPairVector(new String[] { "Placeholder", "Placeholder_1" }, new Tensor[] { a, b }),
+			sess = session.Run(
+					new StringTensorPairVector(new String[] { "Placeholder", "Placeholder_1" }, new Tensor[] { x, y }),
 					new StringVector("Mul"), new StringVector("Mul"), outputs);
 
-			if (!s.ok()) {
-				throw new RuntimeException(s.error_message().getString());
+			if (!sess.ok()) {
+				throw new RuntimeException(sess.error_message().getString());
 			}
 
-			FloatBuffer output = outputs.get(0).createBuffer();
+			DoubleBuffer output = outputs.get(0).createBuffer();
 			for (int i = 0; i < output.limit(); i++) {
 				System.out.println("output: " + output.get(i));
 			}
