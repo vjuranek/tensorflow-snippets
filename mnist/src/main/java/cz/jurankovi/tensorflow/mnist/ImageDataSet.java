e4abe624bc8387ad24b037b101563e44c1b27b98
Vojtech Juranek <vjuranek@redhat.com>
2016-12-25 21:59:53 +0100

    Refactor lables and implement other useful methods

--- mnist/src/main/java/cz/jurankovi/tensorflow/mnist/ImageDataSet.java
+++ mnist/src/main/java/cz/jurankovi/tensorflow/mnist/ImageDataSet.java
@@ -8,14 +8,14 @@ public class ImageDataSet extends DataSet {
 
 	public static final int IMG_MAGIC = 2051;
 
-	private RawImages images;
+	private Images images;
 
 	public ImageDataSet(String path) {
 		super(path);
 		images = path.endsWith(".gz") ? loadDataSetGzip(path) : loadDataSet(path);
 	}
 
-	public RawImages getRawImages() {
+	public Images getImages() {
 		return images;
 	}
 
@@ -23,8 +23,8 @@ public class ImageDataSet extends DataSet {
 		images.print(n);
 	}
 
-	protected RawImages loadDataSet(String path) {
-		RawImages images = null;
+	protected Images loadDataSet(String path) {
+		Images images = null;
 		try (FileInputStream fis = new FileInputStream(path)) {
 			checkMagic(fis, IMG_MAGIC);
 			int numItems = readInt(fis);
@@ -34,7 +34,7 @@ public class ImageDataSet extends DataSet {
 					String.format("Trying to read %d items (with %d rows and %d columns) ...", numItems, nRows, nCols));
 			byte[] imagesRaw = new byte[numItems * nRows * nCols];
 			int read = fis.read(imagesRaw);
-			images = new RawImages(imagesRaw, numItems, nRows, nCols);
+			images = new Images(imagesRaw, numItems, nRows, nCols);
 			LOG.info(String.format("Images read (read %d bytes)", read));
 		} catch (IOException e) {
 			LOG.severe(
@@ -43,8 +43,8 @@ public class ImageDataSet extends DataSet {
 		return images;
 	}
 
-	protected RawImages loadDataSetGzip(String path) {
-		RawImages images = null;
+	protected Images loadDataSetGzip(String path) {
+		Images images = null;
 		try (GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(path), GZIP_BUFF_SIZE)) {
 			checkMagic(gzip, IMG_MAGIC);
 			int numItems = readInt(gzip);
@@ -59,7 +59,7 @@ public class ImageDataSet extends DataSet {
 				System.arraycopy(buff, 0, imagesRaw, read, rb);
 				read += rb;
 			}
-			images = new RawImages(imagesRaw, numItems, nRows, nCols);
+			images = new Images(imagesRaw, numItems, nRows, nCols);
 			LOG.info(String.format("Images read (read %d bytes)", read));
 		} catch (IOException e) {
 			LOG.severe(
@@ -68,21 +68,32 @@ public class ImageDataSet extends DataSet {
 		return images;
 	}
 
-	public class RawImages {
+	public class Images {
 		private final byte[] images;
 		private final int imgCnt;
 		private final int rowCnt;
 		private final int colCnt;
 
-		public RawImages(byte[] images, int imgCnt, int rowCnt, int colCnt) {
+		public Images(byte[] images, int imgCnt, int rowCnt, int colCnt) {
 			this.images = images;
 			this.imgCnt = imgCnt;
 			this.rowCnt = rowCnt;
 			this.colCnt = colCnt;
 		}
 
-		public byte[] getImages() {
+		public byte[] getRawImages() {
 			return images;
+		}
+		
+		public float[] getImagesAsFloat() {
+			float[] asFloat = new float[images.length];
+			for (int i = 0; i < images.length; i++) {
+				asFloat[i] = (float)images[i] < 0 ? ((float)images[i]+ 256)/255 : (float)images[i]/255;
+				if (asFloat[i] > 1.0) { 
+				    throw new IllegalStateException("Bad normalization or data");
+				}
+			}
+			return asFloat;
 		}
 
 		public int getImgCnt() {
@@ -101,8 +112,18 @@ public class ImageDataSet extends DataSet {
 			return reshape2d();
 		}
 		
+		public float[][] get2dFloat() {
+			return reshape2dAsFloat();
+		}
+		
 		public int[][][] get3d() {
 			return reshape3d();
+		}
+		
+		public float[] getBatch(int n) {
+			float[] batch = new float[n * rowCnt * colCnt];
+			System.arraycopy(getImagesAsFloat(), 0, batch, 0, n * rowCnt * colCnt);
+			return batch;
 		}
 		
 		private int[][][] reshape3d() {
@@ -122,6 +143,17 @@ public class ImageDataSet extends DataSet {
 		private int[][] reshape2d() {
 			int pixelCnt = rowCnt * colCnt;
 			int[][] reshaped = new int[imgCnt][pixelCnt];
+			for (int i = 0; i < imgCnt; i++) {
+				for (int j = 0; j < pixelCnt; j++) {
+					reshaped[i][j] = images[i * pixelCnt + j];
+				}
+			}
+			return reshaped;
+		}
+		
+		private float[][] reshape2dAsFloat() {
+			int pixelCnt = rowCnt * colCnt;
+			float[][] reshaped = new float[imgCnt][pixelCnt];
 			for (int i = 0; i < imgCnt; i++) {
 				for (int j = 0; j < pixelCnt; j++) {
 					reshaped[i][j] = images[i * pixelCnt + j];
--- mnist/src/main/java/cz/jurankovi/tensorflow/mnist/LabelDataSet.java
+++ mnist/src/main/java/cz/jurankovi/tensorflow/mnist/LabelDataSet.java
@@ -6,73 +6,123 @@ import java.util.zip.GZIPInputStream;
 
 public class LabelDataSet extends DataSet {
 
-	public static final int LABEL_MAGIC = 2049;
+    public static final int LABEL_MAGIC = 2049;
+    
+    private final Labels labels;
 
-	private final byte[] labels;
+    public LabelDataSet(String path, int numClasses) {
+        super(path);
+        labels = path.endsWith(".gz") ? loadDataSetGzip(path, numClasses) : loadDataSet(path, numClasses);
+    }
 
-	public LabelDataSet(String path) {
-		super(path);
-		labels = path.endsWith(".gz") ? loadDataSetGzip(path) : loadDataSet(path);
-	}
+    public Labels getLabels() {
+        return labels;
+    }
 
-	public byte[] getLabels() {
-		return labels;
-	}
-	
-	public int[][] asOneHot(int numClasses) {
-		int[][] oneHot = new int[labels.length][numClasses];
-		for (int i = 0; i < labels.length; i++) {
-			oneHot[i][labels[i]] = 1;
-		}
-		return oneHot;
-	}
+    public void print(int n) {
+        labels.print(n);
+    }
 
-	public void print(int n) {
-		System.out.println(String.format("First %d labels: ", n));
-		for (int i = 0; i < n; i++) {
-			System.out.print(labels[i] + " ");
-			if ((i + 1) % 10 == 0) {
-				System.out.println();
-			}
-		}
-	}
+    protected Labels loadDataSet(String path, int numClasses) {
+        Labels rawLabels = null;
+        try (FileInputStream fis = new FileInputStream(path)) {
+            checkMagic(fis, LABEL_MAGIC);
+            int numItems = readInt(fis);
+            LOG.info(String.format("Trying to read %d items ...", numItems));
+            byte[] labels = new byte[numItems];
+            int read = fis.read(labels, 0, numItems);
+            LOG.info(String.format("Labels read (read %d bytes)", read));
+            rawLabels = new Labels(labels, numClasses);
+        } catch (IOException e) {
+            LOG.severe(
+                    String.format("Something went wrong, cannot load data set '%s'. Cause: %s", path, e.getMessage()));
+        }
+        return rawLabels;
+    }
 
-	protected byte[] loadDataSet(String path) {
-		byte[] labels = new byte[0];
-		try (FileInputStream fis = new FileInputStream(path)) {
-			checkMagic(fis, LABEL_MAGIC);
-			int numItems = readInt(fis);
-			LOG.info(String.format("Trying to read %d items ...", numItems));
-			labels = new byte[numItems];
-			int read = fis.read(labels, 0, numItems);
-			LOG.info(String.format("Labels read (read %d bytes)", read));
-		} catch (IOException e) {
-			LOG.severe(
-					String.format("Something went wrong, cannot load data set '%s'. Cause: %s", path, e.getMessage()));
-		}
-		return labels;
-	}
+    protected Labels loadDataSetGzip(String path, int numClasses) {
+        Labels rawLabels = null;
+        try (GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(path), GZIP_BUFF_SIZE)) {
+            checkMagic(gzip, LABEL_MAGIC);
+            int numItems = readInt(gzip);
+            LOG.info(String.format("Trying to read %d items ...", numItems));
+            byte[] labels = new byte[numItems];
+            byte[] buff = new byte[GZIP_BUFF_SIZE];
+            int read = 0;
+            int rb = 0;
+            while ((rb = gzip.read(buff)) > -1) {
+                System.arraycopy(buff, 0, labels, read, rb);
+                read += rb;
+            }
+            rawLabels = new Labels(labels, numClasses);
+            LOG.info(String.format("Labels read (read %d bytes)", read));
+        } catch (IOException e) {
+            LOG.severe(
+                    String.format("Something went wrong, cannot load data set '%s'. Cause: %s", path, e.getMessage()));
+        }
+        return rawLabels;
+    }
 
-	protected byte[] loadDataSetGzip(String path) {
-		byte[] labels = new byte[0];
-		try (GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(path), GZIP_BUFF_SIZE)) {
-			checkMagic(gzip, LABEL_MAGIC);
-			int numItems = readInt(gzip);
-			LOG.info(String.format("Trying to read %d items ...", numItems));
-			labels = new byte[numItems];
-			byte[] buff = new byte[GZIP_BUFF_SIZE];
-			int read = 0;
-			int rb = 0;
-			while ((rb = gzip.read(buff)) > -1) {
-				System.arraycopy(buff, 0, labels, read, rb);
-				read += rb;
-			}
-			LOG.info(String.format("Labels read (read %d bytes)", read));
-		} catch (IOException e) {
-			LOG.severe(
-					String.format("Something went wrong, cannot load data set '%s'. Cause: %s", path, e.getMessage()));
-		}
-		return labels;
-	}
+    public class Labels {
+
+        private final byte[] labels;
+        private final int numClasses;
+
+        public Labels(byte[] labels, int numClasses) {
+            this.labels = labels;
+            this.numClasses = numClasses;
+        }
+
+        public byte[] getRawLabels() {
+            return labels;
+        }
+
+        public int getNumClasses() {
+            return numClasses;
+        }
+        
+        public int[][] asOneHot() {
+            int[][] oneHot = new int[labels.length][numClasses];
+            for (int i = 0; i < labels.length; i++) {
+                oneHot[i][labels[i]] = 1;
+            }
+            return oneHot;
+        }
+
+        public float[][] asOneHotFloat() {
+            float[][] oneHot = new float[labels.length][numClasses];
+            for (int i = 0; i < labels.length; i++) {
+                oneHot[i][labels[i]] = 1;
+            }
+            return oneHot;
+        }
+
+        public float[] asOneHotFlatFloat() {
+            float[] batch = new float[labels.length * numClasses];
+            for (int i = 0; i < labels.length; i++) {
+                batch[i * numClasses + labels[i]] = 1;
+            }
+            return batch;
+        }
+
+        public float[] getBatch(int n) {
+            float[] oneHotFlat = new float[n * numClasses];
+            for (int i = 0; i < n; i++) {
+                oneHotFlat[i * numClasses + labels[i]] = 1;
+            }
+            return oneHotFlat;
+        }
+        
+        public void print(int n) {
+            System.out.println(String.format("First %d labels: ", n));
+            for (int i = 0; i < n; i++) {
+                System.out.print(labels[i] + " ");
+                if ((i + 1) % 10 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+
+    }
 
 }
