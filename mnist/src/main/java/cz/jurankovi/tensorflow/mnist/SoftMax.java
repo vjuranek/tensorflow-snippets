7ab9ad5522edf12a820a7f47c16f65722c45bf96
Vojtech Juranek <vjuranek@redhat.com>
2016-12-27 20:02:27 +0100

    Remove unnecessary labels from predicting image

--- mnist/src/main/java/cz/jurankovi/tensorflow/mnist/SoftMax.java
+++ mnist/src/main/java/cz/jurankovi/tensorflow/mnist/SoftMax.java
@@ -1,61 +1,20 @@
 package cz.jurankovi.tensorflow.mnist;
 
 import static org.bytedeco.javacpp.tensorflow.DT_FLOAT;
-import static org.bytedeco.javacpp.tensorflow.DT_STRING;
-import static org.bytedeco.javacpp.tensorflow.InitMain;
-import static org.bytedeco.javacpp.tensorflow.ReadBinaryProto;
 
 import java.nio.FloatBuffer;
 
-import org.bytedeco.javacpp.tensorflow.Env;
-import org.bytedeco.javacpp.tensorflow.GraphDef;
-import org.bytedeco.javacpp.tensorflow.Session;
-import org.bytedeco.javacpp.tensorflow.SessionOptions;
 import org.bytedeco.javacpp.tensorflow.Status;
 import org.bytedeco.javacpp.tensorflow.StringTensorPairVector;
 import org.bytedeco.javacpp.tensorflow.StringVector;
 import org.bytedeco.javacpp.tensorflow.Tensor;
 import org.bytedeco.javacpp.tensorflow.TensorShape;
 import org.bytedeco.javacpp.tensorflow.TensorVector;
-import org.bytedeco.javacpp.helper.tensorflow.StringArray;
 
-public class SoftMax {
-
-	public static final int BATCH_SIZE = 10000;
-	private final String modelPath;
-	private final float[] trainLabels;
-	private final float[] trainImages;
-	private final float[] testLabels;
-    private final float[] testImages;
-    private final int imgPixels;
-    private final int numClasses;
-    
-    private final Session session;
+public class SoftMax extends TFModel {
 
 	public SoftMax(String modelPath, int imgPixels, int numClasses, float[] trainLabesl, float[] trainImages, float[] testLabels, float[] testImages) {
-		this.modelPath = modelPath;
-		this.trainLabels = trainLabesl;
-		this.trainImages = trainImages;
-		this.testLabels = testLabels;
-		this.testImages = testImages;
-		this.imgPixels = imgPixels;
-		this.numClasses = numClasses;
-		
-		InitMain("softmax", (int[]) null, null);
-        GraphDef graph = new GraphDef();
-        ReadBinaryProto(Env.Default(), modelPath, graph);
-        SessionOptions options = new SessionOptions();
-        this.session = new Session(options);
-        Status status = session.Create(graph);
-        checkStatus(status);
-	}
-	
-	public void loadCheckpoint(String checkpointPath) {
-	    Tensor cpPath = new Tensor(DT_STRING, new TensorShape(1));
-        StringArray strArray = cpPath.createStringArray();
-        strArray.position(0).put(checkpointPath); 
-        Status status = session.Run(new StringTensorPairVector(new String[]{"save/Const:0"}, new Tensor[]{cpPath}), new StringVector(), new StringVector("save/restore_all"), new TensorVector());
-        checkStatus(status);
+	    super(modelPath, imgPixels, numClasses, trainLabesl, trainImages, testLabels, testImages);
 	}
 	
 	public void trainModel() {
@@ -69,7 +28,7 @@ public class SoftMax {
         float[] imgBatch = new float[BATCH_SIZE * imgPixels];
         float[] labelBatch = new float[BATCH_SIZE * numClasses];
 
-        int iterations = testImages.length/BATCH_SIZE;
+        int iterations = testImages.length/(BATCH_SIZE * imgPixels);
         for (int i = 0; i < iterations; i++) {
             System.arraycopy(trainImages, i * BATCH_SIZE * imgPixels, imgBatch, 0, BATCH_SIZE * imgPixels);
             System.arraycopy(trainLabels, i * BATCH_SIZE * numClasses, labelBatch, 0, BATCH_SIZE * numClasses);
@@ -118,23 +77,16 @@ public class SoftMax {
 	
 	public void predictImage(int n) {
 	    Tensor x1 = new Tensor(DT_FLOAT, new TensorShape(1, imgPixels));
-        Tensor y1_ = new Tensor(DT_FLOAT, new TensorShape(1, numClasses));
-
         float[] imgTest1 = new float[imgPixels];
-        float[] labelTest1 = new float[numClasses];
         
         System.arraycopy(testImages, imgPixels * (n -1), imgTest1, 0, imgPixels);
-        System.arraycopy(testLabels, numClasses * (n - 1), labelTest1, 0, numClasses);
         FloatBuffer xTest1 = x1.createBuffer();
-        FloatBuffer yTest1 = y1_.createBuffer();
         
         xTest1.limit(imgPixels);
-        yTest1.limit(numClasses);
         xTest1.put(imgTest1);
-        yTest1.put(labelTest1);
         
         TensorVector outputs = new TensorVector();
-        Status status = session.Run(new StringTensorPairVector(new String[] { "x", "y_" }, new Tensor[] { x1, y1_ }),
+        Status status = session.Run(new StringTensorPairVector(new String[] { "x"}, new Tensor[] { x1 }),
                 new StringVector("y"), new StringVector("y"), outputs);
         checkStatus(status);
         
@@ -149,12 +101,6 @@ public class SoftMax {
             }
         }
         System.out.printf("%d test image could be %d", n, maxPos);
-	}
-	
-	private void checkStatus(Status status) {
-	    if (!status.ok()) {
-            throw new RuntimeException(status.error_message().getString());
-        }
 	}
 
 }
